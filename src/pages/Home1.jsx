14a4cbd53ffd2fb092e18654e01102e1326caa8a
karthi srinivasan <94228520+karthi2002@users.noreply.github.com>
2025-07-22 12:37:36 +0530

    commit

--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import shipImg from '../assets/ship.png'
+import waveVideo from '../assets/v1.mp4'
+import VRVideo from '../assets/v2.mp4'
 
 const Home1 = () => {
   return (
-    <div>Home1</div>
+    <div>
+
+    </div>
   )
 }
 
